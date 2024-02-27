aca2fa67094f14e27ddb1f5513921e47c946acb9
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-02-27 11:37:43 +0200

    Update lab2.cpp
    Add a constructor for Staff

--- OOP/lab2.cpp
+++ OOP/lab2.cpp
@@ -6,4 +6,14 @@ public:
     int freecars;
     int priceofdelivery;
 
+    Staff(int valuex, bool boolean) {
+        priceofdelivery = valuex;
+        if (boolean) {
+            freecars = 1;
+        } else {
+            cout << "No cars available" << endl;
+        }
+    }
+
+    
 
